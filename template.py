ff2aaf03f950dc3901fffaa5012e9940e26b81d0
Quentin Agren <quentin.agren@sciencespo.fr>
2022-03-16 16:38:58 +0100

    Add template file

---
+++ template.py
@@ -0,0 +1,17 @@
+from utils import read_input
+
+DAY = 0
+
+def parse(data):
+    pass
+
+def part_one(data: str) -> int:
+    pass
+
+def part_two(data: str) -> int:
+    pass
+
+if __name__ == "__main__":
+    if __name__ == "__main__":
+    data = parse(read_input(day=DAY))
+    part_one(data)
